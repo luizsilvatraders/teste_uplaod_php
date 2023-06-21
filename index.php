fa2454ec016666a740144fb9748cc63275b99c49
lcarlos <luizsilvatraders@hotmail.com>
2023-06-21 19:05:28 -0300

    Cria logica para fazer upload

--- index.php
+++ index.php
@@ -4,10 +4,24 @@
     <title>Upload de Arquivo</title>
 </head>
 <body>
+<?php
+if (isset($_POST["acao"])) {
+    $arquivo = $_FILES["file"];
+    $arquivoNovo = explode('.', $arquivo["name"]);
+
+    if (strtolower($arquivoNovo[sizeof($arquivoNovo) - 1]) !== 'jpg') {
+        die('Você não pode fazer upload deste tipo de arquivo.');
+    } else {
+        echo "Upload feito com sucesso.";
+        move_uploaded_file($arquivo['tmp_name'], 'uploads/' .$arquivo['name']);
+    }
+}
+?>
+
     <h2>Upload de Arquivo</h2>
-    <form action="upload.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="file" multiple>
-        <button>upload</button>
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="file"/>
+        <input type="submit" name="acao" value="enviar"/>
     </form>
 </body>
 </html>
