e5083978aeeacf3d82f23f0cfbbd431ff18590be
lcarlos <luizsilvatraders@hotmail.com>
2023-06-21 18:01:28 -0300

    Cria arquivos e cria estrutura html

--- README.md
+++
@@ -1,1 +0,0 @@
-# teste_uplaod_php
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload de Arquivo</title>
+</head>
+<body>
+    <h2>Upload de Arquivo</h2>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="file" multiple>
+        <button>upload</button>
+    </form>
+</body>
+</html>
