e1c7168dbb41fda2046ba3240287dc5592ba7e8f
lcarlos <luizsilvatraders@hotmail.com>
2023-06-24 14:17:30 -0300

    Cria Front-end da páigina de formulario

--- pagina-de-formulario.php
+++ pagina-de-formulario.php
@@ -1,1 +1,20 @@
-<h1>mfvokewrvofenamo</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulário</title>
+</head>
+<body>
+    <form action="processar-formulario.php" method="POST">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="mensagem">Mensagem:</label>
+        <textarea id="mensagem" name="mensagem" required></textarea>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
